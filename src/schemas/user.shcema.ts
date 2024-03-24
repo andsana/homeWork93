cdb560cb857e8c742e2b1956cea96dbee366fd3c
Susanna Andreeva <sanadex90@gmail.com>
2024-03-25 00:31:53 +0900

    add users.controller.ts, local.strategy.ts, register-user.dto.ts, token-auth.guard.ts

--- src/schemas/user.shcema.ts
+++ src/schemas/user.shcema.ts
@@ -1,6 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
-import bcrypt from 'bcrypt';
+import { compare, genSalt, hash } from 'bcrypt';
 import * as crypto from 'crypto';
 
 const SALT_WORK_FACTOR = 10;
@@ -38,14 +38,14 @@ UserSchema.methods.generateToken = function () {
 };
 
 UserSchema.methods.checkPassword = function (password: string) {
-  return bcrypt.compare(password, this.password);
+  return compare(password, this.password);
 };
 
 UserSchema.pre('save', async function () {
   if (!this.isModified('password')) return;
 
-  const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
-  this.password = await bcrypt.hash(this.password, salt);
+  const salt = await genSalt(SALT_WORK_FACTOR);
+  this.password = await hash(this.password, salt);
 });
 
 UserSchema.set('toJSON', {
---
+++ src/users/users.controller.ts
@@ -0,0 +1,82 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Post,
+  Req,
+  UnauthorizedException,
+  UnprocessableEntityException,
+  UseGuards,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from '../schemas/user.shcema';
+import mongoose, { Model } from 'mongoose';
+import { RegisterUserDto } from './register-user.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+import { TokenAuthGuard } from '../auth/token-auth.guard';
+
+@Controller('users')
+export class UsersController {
+  constructor(
+    @InjectModel(User.name)
+    private userModel: Model<UserDocument>,
+  ) {}
+
+  @Post()
+  async registerUser(@Body() registerUserDto: RegisterUserDto) {
+    try {
+      const user = new this.userModel({
+        email: registerUserDto.email,
+        password: registerUserDto.password,
+        displayName: registerUserDto.displayName,
+      });
+
+      user.generateToken();
+
+      return await user.save();
+    } catch (e) {
+      if (e instanceof mongoose.Error.ValidationError) {
+        throw new UnprocessableEntityException(e);
+      }
+
+      throw e;
+    }
+  }
+
+  @UseGuards(AuthGuard('local'))
+  @Post('sessions')
+  async login(@Req() req: Request) {
+    return { message: 'Correct', user: req.user };
+  }
+
+  @Get()
+  getAll() {
+    return this.userModel.find();
+  }
+
+  @UseGuards(TokenAuthGuard)
+  @Get('secret')
+  async secret(@Req() req: Request) {
+    return {
+      message: 'Secret message',
+      user: req.user,
+    };
+  }
+
+  @UseGuards(TokenAuthGuard)
+  @Delete('logout')
+  async logout(@Req() req: Request) {
+    const user = req.user as UserDocument;
+
+    if (!user) {
+      throw new UnauthorizedException('You are not logged in');
+    }
+
+    user.generateToken();
+    await user.save();
+
+    return { message: 'Successfully logged out' };
+  }
+}
